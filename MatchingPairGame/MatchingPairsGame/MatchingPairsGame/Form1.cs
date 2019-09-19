063c470c97edc04fc85ba8b8d8dc2e167e93f14a
original octopus <jeremypage@nevada.unr.edu>
2019-09-18 20:27:40 -0700

    Project1 Create matching symbols

--- MatchingPairGame/MatchingPairsGame/MatchingPairsGame/Form1.cs
+++ MatchingPairGame/MatchingPairsGame/MatchingPairsGame/Form1.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* Jeremy Page
+ * Reno Technology Academy
+ * CIS109: Coding in C# for Imbedded System
+ * Udemy: 17 Beginner C# Walkthrough Projects Step By Step
+ * Project 1: Create a Matching Pairs Icon Image Game
+ * */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,13 +14,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//Rename the icons and the all the elements we've added to the game
 namespace MatchingPairsGame
 {
     public partial class FormMatchingPairsGame : Form
     {
         public FormMatchingPairsGame()
         {
+            Random random = new Random();
+            List<string> icons = new List<string>()
+            {
+                "!", "!",
+                "q", "q",
+                "?", "?",
+                "zoodle", "zoodle",
+                ",N", ",N",
+                "p", "p",
+                "x", "x",
+                "9", "9",
+            };
             InitializeComponent();
         }
 
