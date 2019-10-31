d886931c33da54e6bb9c78739558ba24a4ca4524
original octopus <jeremypage@nevada.unr.edu>
2019-10-30 19:19:10 -0700

    Finish the Methods

--- Picture Viewer/Picture Viewer/Form1.cs
+++ Picture Viewer/Picture Viewer/Form1.cs
@@ -25,17 +25,18 @@ namespace Picture_Viewer
 
         private void closeButton_Click(object sender, EventArgs e)
         {
-            Close();
+            this.Close(); //this refers to an instance of the program
         }
 
         private void setBackgroundButton_Click(object sender, EventArgs e)
         {
-
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
         }
 
         private void clearPicButton_Click(object sender, EventArgs e)
         {
-
+            pictureBox1.Image = null; //null = empty
         }
 
         private void displayButton_Click(object sender, EventArgs e)
@@ -48,7 +49,10 @@ namespace Picture_Viewer
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else 
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
         }
     }
 }
