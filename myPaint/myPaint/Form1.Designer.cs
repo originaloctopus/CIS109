2f3e2a65761dca2727a812f97cb455abe12854b2
original octopus <jeremypage@nevada.unr.edu>
2019-10-30 21:02:35 -0700

    Adding Code

--- myPaint/myPaint/Form1.Designer.cs
+++ myPaint/myPaint/Form1.Designer.cs
@@ -67,13 +67,13 @@
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
             this.saveAsToolStripMenuItem.Text = "Save As";
             // 
             // saveImageAsToolStripMenuItem
             // 
             this.saveImageAsToolStripMenuItem.Name = "saveImageAsToolStripMenuItem";
-            this.saveImageAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveImageAsToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
             this.saveImageAsToolStripMenuItem.Text = "Save Image As";
             // 
             // toolStrip1
@@ -148,6 +148,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(800, 401);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
--- myPaint/myPaint/Form1.cs
+++ myPaint/myPaint/Form1.cs
@@ -12,9 +12,24 @@ namespace myPaint
 {
     public partial class Form1 : Form
     {
+        Bitmap bmp = new Bitmap(1024, 760);
+        //canvas only of 1024x760 screen size
+
+        Pen p = new Pen(Color.Black, 5);
+        bool drawing = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (drawing)
+                drawing = false;
+            else
+                drawing = true;
+
         }
     }
 }
