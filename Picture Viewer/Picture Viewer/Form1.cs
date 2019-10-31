4033e8e5204a858a390ce70857845a508b819693
original octopus <jeremypage@nevada.unr.edu>
2019-10-30 19:27:33 -0700

    Commenting Our Code

--- Picture Viewer/Picture Viewer/Form1.cs
+++ Picture Viewer/Picture Viewer/Form1.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* Jeremy Page
+ * Reno Technology Academy
+ * CIS109: Coding in C# for Imbedded System
+ * Udemy: 17 Beginner C# Walkthrough Projects Step By Step
+ * Project 1: Create a Matching Pairs Icon Image Game
+ * */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -25,22 +32,28 @@ namespace Picture_Viewer
 
         private void closeButton_Click(object sender, EventArgs e)
         {
-            this.Close(); //this refers to an instance of the program
+            this.Close();
+            //will close the form
         }
 
         private void setBackgroundButton_Click(object sender, EventArgs e)
         {
+            //Show the color dialog box. If the user clicks OK, change the Picturebox control's backgroud to the color the user chose.
             if (colorDialog1.ShowDialog() == DialogResult.OK)
                 pictureBox1.BackColor = colorDialog1.Color;
         }
 
         private void clearPicButton_Click(object sender, EventArgs e)
         {
+            //Will clear the picture.
             pictureBox1.Image = null; //null = empty
         }
 
         private void displayButton_Click(object sender, EventArgs e)
         {
+            /*Shows the open file dialog. If the user clicks okay the picture user selected will be loaded
+              
+            */
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 pictureBox1.Load(openFileDialog1.FileName);
@@ -49,6 +62,10 @@ namespace Picture_Viewer
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
+            /* If the user selects the Streth check box,
+               change the picturebox's sizemode propertry to stretch. If the user clears the checkbox, change it to normal.
+               
+            */
             if (checkBox1.Checked)
                 pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             else 
