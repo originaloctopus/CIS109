3ceffbdd591f8246fcf52b7d007487f302fcb991
original octopus <jeremypage@nevada.unr.edu>
2019-11-06 19:03:48 -0800

    Seconds Label

--- digitalClock/digitalClock/Form1.Designer.cs
+++ digitalClock/digitalClock/Form1.Designer.cs
@@ -29,17 +29,28 @@
         private void InitializeComponent()
         {
             this.hourMinute = new System.Windows.Forms.Label();
+            this.secondsLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // hourMinute
             // 
             this.hourMinute.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hourMinute.ForeColor = System.Drawing.Color.White;
-            this.hourMinute.Location = new System.Drawing.Point(108, 103);
+            this.hourMinute.Location = new System.Drawing.Point(37, 103);
             this.hourMinute.Name = "hourMinute";
             this.hourMinute.Size = new System.Drawing.Size(239, 78);
             this.hourMinute.TabIndex = 0;
             this.hourMinute.Text = "19 : 25";
+            // 
+            // secondsLabel
+            // 
+            this.secondsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondsLabel.ForeColor = System.Drawing.Color.White;
+            this.secondsLabel.Location = new System.Drawing.Point(299, 103);
+            this.secondsLabel.Name = "secondsLabel";
+            this.secondsLabel.Size = new System.Drawing.Size(111, 78);
+            this.secondsLabel.TabIndex = 1;
+            this.secondsLabel.Text = "21";
             // 
             // Form1
             // 
@@ -48,6 +59,7 @@
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(483, 455);
+            this.Controls.Add(this.secondsLabel);
             this.Controls.Add(this.hourMinute);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.Label hourMinute;
+        private System.Windows.Forms.Label secondsLabel;
     }
 }
 
