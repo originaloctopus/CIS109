050989be0acf5230621201ab077c7bb69a0b217f
original octopus <jeremypage@nevada.unr.edu>
2019-11-06 19:05:45 -0800

    Create Date Label

--- digitalClock/digitalClock/Form1.Designer.cs
+++ digitalClock/digitalClock/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.hourMinute = new System.Windows.Forms.Label();
             this.secondsLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // hourMinute
@@ -52,6 +53,16 @@
             this.secondsLabel.TabIndex = 1;
             this.secondsLabel.Text = "21";
             // 
+            // dateLabel
+            // 
+            this.dateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateLabel.ForeColor = System.Drawing.Color.White;
+            this.dateLabel.Location = new System.Drawing.Point(12, 213);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(433, 78);
+            this.dateLabel.TabIndex = 2;
+            this.dateLabel.Text = "Jan 12, 2019";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -59,6 +70,7 @@
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(483, 455);
+            this.Controls.Add(this.dateLabel);
             this.Controls.Add(this.secondsLabel);
             this.Controls.Add(this.hourMinute);
             this.Name = "Form1";
@@ -71,6 +83,7 @@
 
         private System.Windows.Forms.Label hourMinute;
         private System.Windows.Forms.Label secondsLabel;
+        private System.Windows.Forms.Label dateLabel;
     }
 }
 
